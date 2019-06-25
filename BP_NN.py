2263445a4f0f69637b6164a53ae41d95326660a3
sunan <s1142233286@163.com>
2019-06-25 15:14:15 +0800

    构建BP网络，解决异或问题

--- BP_NN.py
+++ BP_NN.py
@@ -15,12 +15,12 @@
 # import lib
 # 输入数据
 import numpy as np
-X = np.array([[1, 0, 1],
+X = np.array([[1, 0, 0],
               [1, 0, 1],
               [1, 1, 0],
               [1, 1, 1]])
 # 标签
-Y = np.array([-1, 1, 1, -1])
+Y = np.array([[0, 1, 1, 0]])
 # 权重初始化，1列3行，取值范围-1,到1
 V = np.random.random((3, 4)) * 2 - 1
 W = np.random.random((4, 1)) * 2 - 1
@@ -32,3 +32,47 @@ lr = 0.11
 n = 0
 # 神经网络输出
 o = 0
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def disigmoid(x):
+    return x*(1 - x)
+
+
+def update():
+    global X, Y, W, V, lr
+
+    L1 = sigmoid(np.dot(X, V))  # 隐藏层输出（4,4）
+    L2 = sigmoid(np.dot(L1, W))  # 输出层输出（4,1）
+    L2_delta = (Y.T - L2) * disigmoid(L2)
+    L1_delta = L2_delta.dot(W.T) * disigmoid(L1)
+    W_C = lr * L1.T.dot(L2_delta)
+    V_C = lr * X.T.dot(L1_delta)
+    W = W + W_C
+    V = V + V_C
+
+
+for i in range(50000):
+    update()
+    if i % 500 == 0:
+        L1 = sigmoid(np.dot(X, V))
+        L2 = sigmoid(np.dot(L1, W))
+        print('Error:', np.mean(np.abs(Y.T - L2)))
+
+L1 = sigmoid(np.dot(X, V))
+L2 = sigmoid(np.dot(L1, W))
+print(L2)
+
+def judge(x):
+    if x>=0.5:
+        return 1
+    else:
+        return 0
+
+for i in map(judge,L2):
+    print(i)
+
+
